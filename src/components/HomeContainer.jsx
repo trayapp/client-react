82de289256929988b5045b207a478ffe54b66eb0
Divuzki <divuzki@gmail.com>
2022-06-10 16:45:43 +0100

    Fixed Bugs

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -15,7 +15,7 @@ const HomeContainer = () => {
         setHeroData(data.heroData);
       }
     });
-  }, []);
+  }, [isLoading]);
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
